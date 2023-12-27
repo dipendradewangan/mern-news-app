2faa4c5df654068fb5c203ec638192c409a37cd3
dipendradewangan <dewangandipendradewangan@gmail.com>
2023-12-27 20:03:17 +0530

    working on cards design

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,187 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
 
 const Home = () => {
     return (
-        <div>
-            home
-        </div>
+        <Fragment>
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
+                <div className='w-full grid grid-cols-3 gap-6 p-6 rounded-lg bg-gray-200'>
+
+                    {/* card */}
+
+                    <div className='bg-white p-6 rounded-md '>
+                        <div className='mb-3 flex justify-between items-center font-bold text-[12px] text-gray-500'>
+                            <div className='flex gap-2 items-center'>
+                                <NewspaperIcon sx={"font-size : 15px"} />
+                                <p>BY ARCHIVE PDF</p>
+                            </div>
+
+                            <div className=''>
+                                <FavoriteBorderIcon sx={"font-size : 15px"} />
+                                112k
+                            </div>
+                        </div>
+                        <img className='rounded-md mb-1' src='https://www.medianews4u.com/wp-content/uploads/2023/12/Malcolm-Raphael-moves-on-from-BCCL-after-15-years-1.jpg' alt='news' />
+                        <div className='flex justify-between font-normal text-gray-600 text-3xl mb-3'>
+                            <h1>Designer Showcase: Peter Do Unapologetically Vietnamese</h1>
+                        </div>
+                        <div className='mb-3'>
+                            <p className='font-[15px]'>
+                                step into the boxing ring: stories stretegies and the unyelilding spirit of champions
+                            </p>
+                        </div>
+                        <div className='flex justify-between font-semibold text-gray-600 text-[10px] '>
+                            <p>2 days ago</p>
+                            <p>November 15th, 2023</p>
+                        </div>
+                    </div>
+
+        
+                    <div className='bg-white p-6 rounded-md '>
+                        <div className='mb-3 flex justify-between items-center font-bold text-[12px] text-gray-500'>
+                            <div className='flex gap-2 items-center'>
+                                <NewspaperIcon sx={"font-size : 15px"} />
+                                <p>BY ARCHIVE PDF</p>
+                            </div>
+
+                            <div className=''>
+                                <FavoriteBorderIcon sx={"font-size : 15px"} />
+                                112k
+                            </div>
+                        </div>
+                        <img className='rounded-md mb-1' src='https://www.medianews4u.com/wp-content/uploads/2023/12/Malcolm-Raphael-moves-on-from-BCCL-after-15-years-1.jpg' alt='news' />
+                        <div className='flex justify-between font-normal text-gray-600 text-3xl mb-3'>
+                            <h1>Designer Showcase: Peter Do Unapologetically Vietnamese</h1>
+                        </div>
+                        <div className='mb-3'>
+                            <p className='font-[15px]'>
+                                step into the boxing ring: stories stretegies and the unyelilding spirit of champions
+                            </p>
+                        </div>
+                        <div className='flex justify-between font-semibold text-gray-600 text-[10px] '>
+                            <p>2 days ago</p>
+                            <p>November 15th, 2023</p>
+                        </div>
+                    </div>
+
+
+                    
+                    <div className='bg-white p-6 rounded-md '>
+                        <div className='mb-3 flex justify-between items-center font-bold text-[12px] text-gray-500'>
+                            <div className='flex gap-2 items-center'>
+                                <NewspaperIcon sx={"font-size : 15px"} />
+                                <p>BY ARCHIVE PDF</p>
+                            </div>
+
+                            <div className=''>
+                                <FavoriteBorderIcon sx={"font-size : 15px"} />
+                                112k
+                            </div>
+                        </div>
+                        <img className='rounded-md mb-1' src='https://www.medianews4u.com/wp-content/uploads/2023/12/Malcolm-Raphael-moves-on-from-BCCL-after-15-years-1.jpg' alt='news' />
+                        <div className='flex justify-between font-normal text-gray-600 text-3xl mb-3'>
+                            <h1>Designer Showcase: Peter Do Unapologetically Vietnamese</h1>
+                        </div>
+                        <div className='mb-3'>
+                            <p className='font-[15px]'>
+                                step into the boxing ring: stories stretegies and the unyelilding spirit of champions
+                            </p>
+                        </div>
+                        <div className='flex justify-between font-semibold text-gray-600 text-[10px] '>
+                            <p>2 days ago</p>
+                            <p>November 15th, 2023</p>
+                        </div>
+                    </div>
+
+
+                    
+                    <div className='bg-white p-6 rounded-md '>
+                        <div className='mb-3 flex justify-between items-center font-bold text-[12px] text-gray-500'>
+                            <div className='flex gap-2 items-center'>
+                                <NewspaperIcon sx={"font-size : 15px"} />
+                                <p>BY ARCHIVE PDF</p>
+                            </div>
+
+                            <div className=''>
+                                <FavoriteBorderIcon sx={"font-size : 15px"} />
+                                112k
+                            </div>
+                        </div>
+                        <img className='rounded-md mb-1' src='https://www.medianews4u.com/wp-content/uploads/2023/12/Malcolm-Raphael-moves-on-from-BCCL-after-15-years-1.jpg' alt='news' />
+                        <div className='flex justify-between font-normal text-gray-600 text-3xl mb-3'>
+                            <h1>Designer Showcase: Peter Do Unapologetically Vietnamese</h1>
+                        </div>
+                        <div className='mb-3'>
+                            <p className='font-[15px]'>
+                                step into the boxing ring: stories stretegies and the unyelilding spirit of champions
+                            </p>
+                        </div>
+                        <div className='flex justify-between font-semibold text-gray-600 text-[10px] '>
+                            <p>2 days ago</p>
+                            <p>November 15th, 2023</p>
+                        </div>
+                    </div>
+
+
+                    
+                    <div className='bg-white p-6 rounded-md '>
+                        <div className='mb-3 flex justify-between items-center font-bold text-[12px] text-gray-500'>
+                            <div className='flex gap-2 items-center'>
+                                <NewspaperIcon sx={"font-size : 15px"} />
+                                <p>BY ARCHIVE PDF</p>
+                            </div>
+
+                            <div className=''>
+                                <FavoriteBorderIcon sx={"font-size : 15px"} />
+                                112k
+                            </div>
+                        </div>
+                        <img className='rounded-md mb-1' src='https://www.medianews4u.com/wp-content/uploads/2023/12/Malcolm-Raphael-moves-on-from-BCCL-after-15-years-1.jpg' alt='news' />
+                        <div className='flex justify-between font-normal text-gray-600 text-3xl mb-3'>
+                            <h1>Designer Showcase: Peter Do Unapologetically Vietnamese</h1>
+                        </div>
+                        <div className='mb-3'>
+                            <p className='font-[15px]'>
+                                step into the boxing ring: stories stretegies and the unyelilding spirit of champions
+                            </p>
+                        </div>
+                        <div className='flex justify-between font-semibold text-gray-600 text-[10px] '>
+                            <p>2 days ago</p>
+                            <p>November 15th, 2023</p>
+                        </div>
+                    </div>
+
+                    
+                    <div className='bg-white p-4 rounded-md '>
+                        <div className='mb-3 flex justify-between items-center font-bold text-[12px] text-gray-500'>
+                            <div className='flex gap-2 items-center'>
+                                <NewspaperIcon sx={"font-size : 15px"} />
+                                <p>BY ARCHIVE PDF</p>
+                            </div>
+
+                            <div className=''>
+                                <FavoriteBorderIcon sx={"font-size : 15px"} />
+                                112k
+                            </div>
+                        </div>
+                        <img className='rounded-md mb-1' src='https://www.medianews4u.com/wp-content/uploads/2023/12/Malcolm-Raphael-moves-on-from-BCCL-after-15-years-1.jpg' alt='news' />
+                        <div className='flex justify-between font-normal text-gray-600 text-3xl mb-3'>
+                            <h1>Designer Showcase: Peter Do Unapologetically Vietnamese</h1>
+                        </div>
+                        <div className='mb-3'>
+                            <p className='font-[15px]'>
+                                step into the boxing ring: stories stretegies and the unyelilding spirit of champions
+                            </p>
+                        </div>
+                        <div className='flex justify-between font-semibold text-gray-600 text-[10px] '>
+                            <p>2 days ago</p>
+                            <p>November 15th, 2023</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
     )
 }
 
