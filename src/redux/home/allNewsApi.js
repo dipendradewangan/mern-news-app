ccd9178a8132377ba1933df1f4fd6f8bed73a894
dipendradewangan <dewangandipendradewangan@gmail.com>
2024-01-03 22:26:55 +0530

    removed unwanted console log

--- src/redux/home/allNewsApi.js
+++ src/redux/home/allNewsApi.js
@@ -1,6 +1,5 @@
 export const fetchAllNews = (country, title)=>{
     return new Promise(async (resolve)=>{
-        // console.log(title)
         const response = await fetch(`https://newsapi.org/v2/top-headlines?category=${title}&country=${country}&language=en&apiKey=5de847037bef4587848eccc10017bd3a`)
         const data = await response.json()
         resolve({data : data.articles})
